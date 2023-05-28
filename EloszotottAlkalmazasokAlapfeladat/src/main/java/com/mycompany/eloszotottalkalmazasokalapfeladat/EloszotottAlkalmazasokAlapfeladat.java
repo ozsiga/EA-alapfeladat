bdf16d05735161c7349ad0bd7e9430a5a312d62b
Z$1GA <Z$1GA@DESKTOP-FK6RQ4Q>
2023-05-28 12:39:56 +0200

    alapfeladat main file - elso commit - WIP

---
+++ EloszotottAlkalmazasokAlapfeladat/src/main/java/com/mycompany/eloszotottalkalmazasokalapfeladat/EloszotottAlkalmazasokAlapfeladat.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eloszotottalkalmazasokalapfeladat;
+
+/**
+ *
+ * @author Z$1GA
+ */
+public class EloszotottAlkalmazasokAlapfeladat {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
